36929036707a5906f468e0798181a4130b0d16ac
jorgeelopezm <147439695+jorgeelopezm@users.noreply.github.com>
2026-08-21 11:03:50 -0300

    Mejoras en documentación y se agrega ps para levantar en local con un solo comando

---
+++ levantar-entorno.ps1
@@ -0,0 +1,3 @@
+start powershell {.\dockernpm.ps1; Read-Host};
+
+start powershell {.\uvicorn.ps1; Read-Host};
---
+++ uvicorn.ps1
@@ -0,0 +1,4 @@
+#Levanta Uvicorn
+cd C:\Users\jorge\Documents\desarrollo-ia-bios\entregable-2-jorge-lopez\api\
+.venv\Scripts\Activate.ps1
+uvicorn app.main:app --reload;
